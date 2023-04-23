856cf21030e703eda8ba0f01a8b02b8e95011db8
Arsh Ahmad <123231460+Ahmad-Arsh@users.noreply.github.com>
2023-04-23 13:13:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let element = document.querySelector('#level');
+let level = 0;
+while (element.parentNode) {
+    level++;
+    element = element.parentNode;
+}
+alert(`The level of the element is: ${level}`);
